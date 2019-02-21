4e625e8df8e410e1ac750f2b4c5e64e235afd663
walter <walter@walter9088.local>
2019-02-21 14:58:26 +0800

    up

--- companInfoEtl.py
+++ companInfoEtl.py
@@ -8,15 +8,8 @@ import re
 import jieba.analyse
 
 
-
-
-
-
-list_keys=[]
-
-list_in_keys = []
-
-list_co_keys = []
+g_dict ={}
+c_dict = {}
 
 jieba.analyse.set_stop_words("data/stopword.txt")
 jieba.load_userdict("data/dict.txt")
@@ -26,19 +19,39 @@ def readInFile():
     data = xlrd.open_workbook("data/t.xls")
 
     table = data.sheet_by_index(0)
+    old_str_v_cell3 = ""
+    for i in range(6, 1167):
 
-    for i in range(0, 1167):
+        v_cell3 = table.cell(i, 3).value
+        str_v_cell3 =''
 
-        v_cell2 = table.cell(i, 5).value
-        if isinstance(v_cell2,unicode):
-            match = re.sub(ur'\w', "", v_cell2)
+        v_cell4 = ''
+        v_cell5 =''
+
+        if v_cell3 != "":
+            if isinstance(v_cell3,unicode):
+                str_v_cell3 = str(v_cell3)
+
+            if isinstance(v_cell3,float):
+                str_v_cell3 = str(int(v_cell3))
+
+        if old_str_v_cell3 != str_v_cell3:
+            v_cell4 = table.cell(i, 4).value
+            v_cell5 = table.cell(i, 5).value
+            old_str_v_cell3 = str_v_cell3
+        else:
+            continue;
+
+
+        if isinstance(v_cell5,unicode):
+            match = re.sub(ur'\w', "", v_cell5)
             keywords = jieba.analyse.extract_tags(match, topK=20, withWeight=True, allowPOS=('ns', 'n', 'vn', 'v'))
-            # 访问提取结果
+            # # 访问提取结果
+            list_g_keys = []
             for item in keywords:
-                list_keys.append(item[0])
-                list_in_keys.append(item[0])
+                list_g_keys.append(item[0])
 
-
+            g_dict[v_cell4] = list_g_keys
 
 
 def readCompanyFile():
@@ -48,14 +61,18 @@ def readCompanyFile():
 
     for i in range(0, 10000):
 
+        v_cell1 = table.cell(i, 0).value
         v_cell2 = table.cell(i, 2).value
+
         if isinstance(v_cell2,unicode):
             match = re.sub(ur'[\(（\[].*[\)）\]]', "", v_cell2)
             keywords = jieba.analyse.extract_tags(match, topK=20, withWeight=True, allowPOS=('ns', 'n', 'vn', 'v'))
+            list_c_keys = []
             # 访问提取结果
             for item in keywords:
-                list_keys.append(item[0])
-                list_co_keys.append(item[0])
+                list_c_keys.append(item[0])
+            c_dict[v_cell1] = list_c_keys
+
 
 
 def createFile(list_keys,string):
@@ -68,15 +85,28 @@ def createFile(list_keys,string):
 
     dict_f.close()
 
+
 if __name__ == "__main__":
 
    readInFile()
 
    readCompanyFile()
 
-   createFile(list_in_keys,'dict_in.txt')
-   createFile(list_co_keys, 'dict_co.txt')
-   createFile(list_keys, 'dict.txt')
+   for (c_k,c_v) in c_dict.items():
+
+       for (g_k,g_v) in g_dict.items():
+           r_l = list((set(c_v).union(set(g_v)))^(set(c_v)^set(g_v)))
+           if len(r_l)>0:
+            #print str(c_v).decode('unicode-escape'),str(g_v).decode('unicode-escape'),str(r_l).decode('unicode-escape')
+            print u"公司:========"+c_k+u":===========,在国民经济行业：========"+g_k+u"：========下有关键词匹配成功能，关键词是："+str(r_l).decode('unicode-escape')
+
+
+
+
+
+   #createFile(list_in_keys,'dict_in.txt')
+   #createFile(list_co_keys, 'dict_co.txt')
+   #createFile(list_keys, 'dict.txt')
 
 
 
